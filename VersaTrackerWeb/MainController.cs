b2daeecf3a308a3a41958c5a3f2b93423df33923
Evgeny 'Riketta' Chugaev <rowneg@bk.ru>
2021-03-02 14:02:11 +0300

    added base web UI project, currently in-dev

---
+++ VersaTrackerWeb/MainController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace VersaTrackerWeb
+{
+    public class MainController : Controller
+    {
+        public string Index()
+        {
+            return "POLUHUI";
+        }
+
+        public string Search(int id, string item = "")
+        {
+            return HtmlEncoder.Default.Encode($"Searching lots for realm id {id} with name \"{item}\"");
+        }
+    }
+}
